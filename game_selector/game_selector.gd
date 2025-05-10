7ce4c2736e809ce5ca4ab8fded638bc5dffbb9d3
Patrick W. Crawford <theduckcow@live.com>
2025-05-09 22:35:45 -0700

    Exclude the DS_Store file after the pack is extracted

--- game_selector/game_selector.gd
+++ game_selector/game_selector.gd
@@ -17,7 +17,7 @@ func get_levels(path) -> void:
 		while file_name != "":
 			if dir.current_is_dir():
 				pass
-			elif file_name.get_extension() == "remap":
+			elif file_name.get_extension() in ["remap", "DS_Store"]:
 				pass
 			else:
 				create_level_btn("%s/%s" % [dir.get_current_dir(), file_name], file_name)
